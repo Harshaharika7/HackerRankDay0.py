487cc8f88b8e32fa42002ffee1acde0af5ba0e5e
Harshaharika7 <138356696+Harshaharika7@users.noreply.github.com>
2025-05-17 09:55:05 +0530

    Create Hello_world.py

---
+++ Hello_world.py
@@ -0,0 +1,8 @@
+# Read a full line of input from stdin and save it to the variable 'input_string'.
+input_string = input()
+
+# Print the string literal "Hello, World." on the first line
+print("Hello, World.")
+
+# Print the contents of 'input_string' on the second line
+print(input_string)
